df1d01319c4af44cfc18120b29bbeaa2ba566064
Akhileshwar Shriram <112577383+AkhilTheBoss@users.noreply.github.com>
2023-03-17 15:17:31 -0700

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -23,6 +23,22 @@ function Map() {
           <div
             style={{
               display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            <iframe
+              title="UC Davis Map"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12023.14292712881!2d-121.74989339768725!3d38.538212149965034!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa5139d7e329a08c!2sUC%20Davis!5e0!3m2!1sen!2sus!4v1647583037002!5m2!1sen!2sus!maptype=satellite"
+              width="800"
+              height="600"
+              style={{ border: "0", borderRadius: "10px" }}
+            ></iframe>
+          </div>
+          <div
+            style={{
+              display: "flex",
               justifyContent: "space-around",
               width: "100%",
               marginTop: "20px",
@@ -65,23 +81,6 @@ function Map() {
               <button>About Unitrans</button>
             </div>
           </div>
-
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100vh",
-            }}
-          >
-            <iframe
-              title="UC Davis Map"
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12023.14292712881!2d-121.74989339768725!3d38.538212149965034!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa5139d7e329a08c!2sUC%20Davis!5e0!3m2!1sen!2sus!4v1647583037002!5m2!1sen!2sus!maptype=satellite"
-              width="800"
-              height="600"
-              style={{ border: "0", borderRadius: "10px" }}
-            ></iframe>
-          </div>
         </>
       ) : (
         <>
@@ -94,3 +93,4 @@ function Map() {
 }
 
 export default Map;
+
